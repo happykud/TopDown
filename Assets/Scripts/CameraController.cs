79f9c5a68269dccf6f4d65581dacd0e13b196762
happykud <kyhaa71455@gmail.com>
2024-05-14 17:25:26 +0900

    kjh

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,7 +8,14 @@ public class CameraController : MonoBehaviour
 
     public GameObject player;
 
-    private void Update()
+    //private void Update()
+    //{
+    //    //Vector3 dir = player.transform.position - this.transform.position;
+    //    //Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+    //    //this.transform.Translate(moveVector);
+    //}
+
+    private void LateUpdate() // 물리 처리 후 업데이트 되는? 업데이트 중 가장 뒤? (렌더링, 카메라) 암튼 보여주는거, 내보내는거
     {
         Vector3 dir = player.transform.position - this.transform.position;
         Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
