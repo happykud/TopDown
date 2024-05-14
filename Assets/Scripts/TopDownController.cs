97030f5925db765874071bb0675a9d16f086d755
happykud <kyhaa71455@gmail.com>
2024-05-14 09:03:27 +0900

    Initial commit

---
+++ Assets/Scripts/TopDownController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+    
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+    //몬스터는 위 부분이 달라짐 (하나의 스크립트로 몬스터도 같이 사용할수 있는 구조)
+}
---
+++ Assets/Scripts/TopDownMonement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    // 실제로 이동이 일어나는 부분
+
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    [SerializeField] private int speed = 5;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMonement(movementDirection);
+    }
+
+    private void ApplyMonement(Vector2 direction)
+    {
+        direction = direction * speed;
+        movementRigidbody.velocity = direction;
+    }
+}
